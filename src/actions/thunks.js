51d8ed92c080e6767a3b713dff57b30e387c1fda
Jennifer Lusong <jenniferlusong@gmail.com>
2020-10-17 20:31:40 +0800

    Initial commit

---
+++ src/actions/thunks.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import {
+  isFetching,
+  doneFetching,
+  fetchCountry,
+  fetchSelectedCountry,
+  fetchGlobalCovid
+} from "./index";
+
+export const fetchCountryAction = () => async dispatch => {
+  dispatch(isFetching());
+  try {
+    const response = await axios.get("https://restcountries.eu/rest/v2/all");
+    const { data } = response;
+    dispatch(fetchCountry(data));
+    dispatch(doneFetching());
+  } catch (error) {
+    dispatch(doneFetching());
+  }
+};
+
+export const fetchGlobalCovidAction = () => async dispatch => {
+  dispatch(isFetching());
+  try {
+    const response = await axios.get("https://api.covid19api.com/summary");
+    const { data } = response;
+    dispatch(fetchGlobalCovid(data));
+    dispatch(fetchCountry(data));
+    dispatch(doneFetching());
+  } catch (error) {
+    dispatch(doneFetching());
+  }
+};
+
+export const fetchSelectedCountryAction = country => async dispatch => {
+  dispatch(isFetching());
+  try {
+    const response = await axios.get(
+      `https://api.covid19api.com/total/country/${country}`
+    );
+    const { data } = response;
+    dispatch(fetchSelectedCountry({ data, country }));
+    dispatch(doneFetching());
+  } catch (error) {
+    dispatch(doneFetching());
+  }
+};
